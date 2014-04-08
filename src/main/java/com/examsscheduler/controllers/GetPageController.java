c05b320f5ab82819acd2ec3609421c735a02066c
Serg <tojstein@gmail.com>
2014-04-08 15:45:29 +0300

    Add pages controller. Mock login page

---
+++ src/main/java/com/examsscheduler/controllers/GetPageController.java
@@ -0,0 +1,16 @@
+package com.examsscheduler.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/pages")
+public class GetPageController {
+	
+	@RequestMapping(value="/loginPage", method=RequestMethod.GET)
+	public String getMainPage(){
+		return "loginPage";
+	}
+	
+}
---
+++ src/test/java/com/examsscheduler/controllers/GetPageControllerTest.java
@@ -0,0 +1,30 @@
+package com.examsscheduler.controllers;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+
+import com.examsscheduler.controllers.GetPageController;
+
+public class GetPageControllerTest {
+	
+	@Mock
+	private GetPageController controller;
+
+	@Before
+	public void setup(){
+		controller = new GetPageController();
+	}
+	
+	@Test
+	public void shouldGetPage(){
+		assertEquals(controller.getMainPage(), "loginPage");
+	}
+	
+	
+	
+	
+
+}
