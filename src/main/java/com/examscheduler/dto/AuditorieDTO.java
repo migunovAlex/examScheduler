c2a40766053ee942790ae64606e81cb56af546e5
tojstein <tojstein@gmail.com>
2015-08-04 08:34:21 +0400

    US-27: Added new files for auditories

---
+++ src/main/java/com/examscheduler/dto/AuditorieDTO.java
@@ -0,0 +1,29 @@
+package com.examscheduler.dto;
+
+public class AuditorieDTO {
+	
+	private int id;
+	private String number;
+	private int maxPerson;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNumber() {
+		return number;
+	}
+	public void setNumber(String number) {
+		this.number = number;
+	}
+	public int getMaxPerson() {
+		return maxPerson;
+	}
+	public void setMaxPerson(int maxPerson) {
+		this.maxPerson = maxPerson;
+	}
+	
+	
+}
